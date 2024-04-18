992ef8f97f8729e01fa0a5d90257a7eb63011f7d
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-04-18 09:44:21 -0400

    connected db

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import service.Database;
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Database.getConnection(); // test connection
+    }
+}
